cdebada434619e58f1712c6e8f5913d08bf2fed0
Elena Nikolaeva <elena.nikolaeva@booking.com>
2019-01-29 15:35:13 +0100

    added distance calculation to geometry/Main.java

--- src/main/java/geometry/Main.java
+++ src/main/java/geometry/Main.java
@@ -2,6 +2,13 @@ package geometry;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+
+        double lat1NorthNetherlands = 53.478612, lon1NorthNetherlands = 6.250578;
+        double lat2SouthNetherlands = 50.752342, lon2SouthNetherlands = 5.916981;
+        double distance = GeometryHelpers.getDistance(
+                lat1NorthNetherlands, lon1NorthNetherlands,
+                lat2SouthNetherlands, lon2SouthNetherlands);
+
+        System.out.print(String.format("Distance from the north to the south of the Netherlands: %.2f meters", distance, " meters"));
     }
 }
